3301f81bddeacf6c7cd5db1c9bbc2c2a5cec0365
Kelly Selden <kellyselden@gmail.com>
2015-09-09 17:40:00 -0400

    initial commit

---
+++ GenericExtensions.Tests/ICollectionExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace GenericExtensions.Tests
+{
+	[TestClass]
+	public class ICollectionExtensionsTests
+	{
+		[TestMethod]
+		public void ICollectionExtensions_Add()
+		{
+			var list = new List<int>(new[] { 0, 1 });
+
+			list.Add(new[] { 2, 3 });
+
+			AssertHelper.AreDeeplyEqual(new[] { 0, 1, 2, 3 }, list);
+		}
+
+		[TestMethod]
+		public void ICollectionExtensions_Remove()
+		{
+			var list = new List<int>(new[] { 0, 1, 2, 3 });
+
+			list.Remove(x => x == 2 || x == 3);
+
+			AssertHelper.AreDeeplyEqual(new[] { 0, 1 }, list);
+		}
+
+		[TestMethod]
+		public void ICollectionExtensions_RemoveWithIndex()
+		{
+			var list = new List<int>(new[] { 0, 1, 2, 3 });
+
+			list.Remove((x, i) => i == 2 || i == 3);
+
+			AssertHelper.AreDeeplyEqual(new[] { 0, 1 }, list);
+		}
+	}
+}
---
+++ GenericExtensions/ICollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericExtensions
+{
+	public static class ICollectionExtensions
+	{
+		public static ICollection<TSource> Add<TSource>(this ICollection<TSource> source, IEnumerable<TSource> collection)
+		{
+			foreach (TSource element in collection)
+			{
+				source.Add(element);
+			}
+
+			return source;
+		}
+
+		public static ICollection<TSource> Remove<TSource>(this ICollection<TSource> source, Func<TSource, bool> predicate)
+		{
+			foreach (TSource element in source.Where(predicate).ToArray())
+			{
+				source.Remove(element);
+			}
+
+			return source;
+		}
+
+		public static ICollection<TSource> Remove<TSource>(this ICollection<TSource> source, Func<TSource, int, bool> predicate)
+		{
+			foreach (TSource element in source.Where(predicate).ToArray())
+			{
+				source.Remove(element);
+			}
+
+			return source;
+		}
+	}
+}
